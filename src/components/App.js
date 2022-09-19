b58526ad0ce4389938d82d9f5671f1eabcd92459
Tristen <tristen.hammon21@gmail.com>
2022-09-19 09:30:21 -0500

    commit tree 2629b0b51ee930e8c95ef99e1d4fd9a13e14f4ff

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+function App(){
+  return(
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+    </div>
+  )
+}
+
 function NavBar() {
   return (
     <nav>
@@ -16,17 +26,13 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
-
-function App() {
-  return (
-    <div>
-      <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+function About() {
+  return(
+    <div id= "about">
+      <h2>About</h2>
     </div>
-  );
+  )
 }
+
 
 export default App;
